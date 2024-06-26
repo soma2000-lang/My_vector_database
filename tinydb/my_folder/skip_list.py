1b8c836b52482fbd4e6ec4ac78e8771f5429379f
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 22:44:41 +0530

    Update at Wed, 26 Jun 2024 17:14:41 GMT

--- tinydb/my_folder/skip_list.py
+++ tinydb/my_folder/skip_list.py
@@ -10,3 +10,18 @@ class Node:
 
     def __repr__(self) -> str:
         return str(self.value)
+    def __init__(
+        self, lst: list[int] | None = None, max_level: int = 2, p: float = 0.5
+    ) -> None:
+        assert max_level >= 0
+
+        self.max_level = max_level  # note: max_level is 0-indexed (0 means 1 level, 1 means 2 levls, etc.)
+        self.level = 0
+        self.p = p
+        self.header = Node(value=-1, level=self.max_level)
+
+        if lst is None:
+            lst = []
+
+        for value in lst:
+            self.insert(value)
