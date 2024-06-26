3fe0f6892e683ac282a0801b43528b084ed70a7d
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 21:51:52 +0530

    Update at Wed, 26 Jun 2024 16:21:52 GMT

--- tinydb/my_folder/nsw.py
+++ tinydb/my_folder/nsw.py
@@ -2,3 +2,15 @@ import numpy as np
 
 from tinydb.utils import evaluate, load_sift
 from tinydb.knn import cosine_similarity
+
+
+class NSWIndex:
+    def __init__(self):
+        self.graph = {}  # Dictionary to store the graph
+        self.data = []  # List to store the actual data items
+
+    def add_item(self, item, f=10, w=5):
+        # Add item to data list
+        self.data.append(item)
+        idx = len(self.data) - 1
+
