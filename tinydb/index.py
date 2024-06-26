b3bf7f864b4dbcc4a43f818edd1c25d24b3a8bb2
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 20:48:25 +0530

    Update at Wed, 26 Jun 2024 15:18:25 GMT

--- tinydb/index.py
+++ tinydb/index.py
@@ -18,3 +18,21 @@ class Index:
             self.f_distance = l2_distance
         elif distance == "inner_product":
             self.f_distance = inner_product_distance
+    def add(self, vectors: numpy.ndarray) -> None:
+            assert vectors.shape[1] == self.d
+
+        if self.vectors is None:
+            self.vectors = vectors
+            self.is_trained = True
+        else:
+            self.vectors = numpy.append(self.vectors, vectors, axis=0)
+
+        self.ntotal = self.vectors.shape[0]
+    def search(
+        self, query: numpy.ndarray, k: int
+    ) -> tuple[numpy.ndarray, numpy.ndarray]:
+        raise NotImplementedError()
+
+    def save(self, file: str) -> None:
+        with open(file, "wb") as f:
+            pickle.dump(self, f)
