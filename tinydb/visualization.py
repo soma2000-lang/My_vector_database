4c519872270c91fcd4b2faa1a50f8b5ca66889ad
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 21:24:06 +0530

    Update at Wed, 26 Jun 2024 15:54:06 GMT

--- tinydb/visualization.py
+++ tinydb/visualization.py
@@ -10,3 +10,9 @@ import matplotlib.pyplot as plt
 
 
 Layout = Dict[int, Tuple[float, float]]
+def visualize_hnsw_index(index: HNSWIndex, layout: Optional[Layout] = None) -> None:
+    """
+    Use this to visualize the different layers of HNSW graphs. The nodes
+    maintain consistent locations between layers, and the layers are
+    plotted next to each other.
+    """
