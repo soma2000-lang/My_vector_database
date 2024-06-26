9356a5b221bb310d8ae833d91f076c1ef5328991
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 21:16:29 +0530

    Update at Wed, 26 Jun 2024 15:46:29 GMT

--- tinydb/utils.py
+++ tinydb/utils.py
@@ -6,7 +6,6 @@ import urllib.request as request
 
 from contextlib import closing
 
-from contextlib import closing
 
 
 DATA_PATH = os.path.join("data", "siftsmall", "siftsmall_base.fvecs")
@@ -14,12 +13,8 @@ QUERY_PATH = os.path.join("data", "siftsmall", "siftsmall_query.fvecs")
 LABEL_PATH = os.path.join("data", "siftsmall", "siftsmall_groundtruth.ivecs")
 
 
-def read_vecs(path: str, ivecs: bool = False) -> numpy.ndarray:
-    a = numpy.fromfile(path, dtype="int32")
-    d = a[0]
-    matrix = a.reshape(-1, d + 1)[:, 1:].copy()
-
-    if not ivecs:
-        matrix = matrix.view("float32")
-
-    return matrix
+def download_sift():
+    output= os.path.join("data", "siftsmall.tar.gz")
+    with closing(
+        request.urlopen("ftp://ftp.irisa.fr/local/texmex/corpus/siftsmall.tar.gz")
+    ) as r:
