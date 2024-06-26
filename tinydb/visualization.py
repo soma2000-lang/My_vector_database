a0f2ba323043dbf8cd59afe9c6cac22e2f7c5a7b
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 21:26:48 +0530

    Update at Wed, 26 Jun 2024 15:56:48 GMT

--- tinydb/visualization.py
+++ tinydb/visualization.py
@@ -16,3 +16,11 @@ def visualize_hnsw_index(index: HNSWIndex, layout: Optional[Layout] = None) -> N
     maintain consistent locations between layers, and the layers are
     plotted next to each other.
     """
+    _, axs = plt.subplots(1, len(index.layers), figsize=(len(index.layers) * 5, 5))
+l   layout = layout or networkx.spring_layout(index.layers[0].G)
+    node_color = ["r" if index.ep == node else "c" for node, _ in layout.items()]
+    # Determine the global min and max coordinates for consistent axes
+    all_x_values = [pos[0] for pos in layout.values()]
+    all_y_values = [pos[1] for pos in layout.values()]
+    min_x, max_x = min(all_x_values), max(all_x_values)
+    min_y, max_y = min(all_y_values), max(all_y_values)
