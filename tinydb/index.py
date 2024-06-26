791aa14f4671749129ccb25488fc8eca6666ebf3
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 20:47:25 +0530

    Update at Wed, 26 Jun 2024 15:17:25 GMT

--- tinydb/index.py
+++ tinydb/index.py
@@ -2,3 +2,19 @@ from __future__ import annotations
 
 import numpy
 import pickle
+
+class Index:
+    def __init__(self, d: int, distance: str = "cosine") -> None:
+        self.ntotal = 0
+        self.vectors = None
+        self.is_trained = False
+        self.d = d
+
+        assert distance in ["cosine", "l2", "inner_product"]
+
+        if distance == "cosine":
+            self.f_distance = cosine_distance
+        elif distance == "l2":
+            self.f_distance = l2_distance
+        elif distance == "inner_product":
+            self.f_distance = inner_product_distance
