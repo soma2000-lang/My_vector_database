f50f9b869f95d89b7081f22b0d0d4e034d15f411
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 20:32:32 +0530

    Update at Wed, 26 Jun 2024 15:02:32 GMT

--- tinydb/filter.py
+++ tinydb/filter.py
@@ -2,3 +2,30 @@ from __future__ import annotations
 
 import numpy
 import pickle
+
+
+class Index:
+    def __init__(self, d: int, distance: str = "cosine") -> None:
+        self.ntotal = 0
+        self.vectors = None
+        self.is_trained = False
+        self.d = d
+
+        assert distance in ["cosine", "l2", "inner_product"]
+
+        if distance == "cosine":
+            self.f_distance = cosine_distance
+        elif distance == "l2":
+            self.f_distance = l2_distance
+        elif distance == "inner_product":
+            self.f_distance = inner_product_distance
+    def add(self, vectors: numpy.ndarray) -> None:
+        assert vectors.shape[1] == self.d
+
+        if self.vectors is None:
+            self.vectors = vectors
+            self.is_trained = True
+        else:
+            self.vectors = numpy.append(self.vectors, vectors, axis=0)
+
+        self.ntotal = self.vectors.shape[0]
