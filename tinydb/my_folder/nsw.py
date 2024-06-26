9169c91da382c0a552dad1b506e1caa5e48c5bfc
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 22:01:39 +0530

    Update at Wed, 26 Jun 2024 16:31:39 GMT

--- tinydb/my_folder/nsw.py
+++ tinydb/my_folder/nsw.py
@@ -35,3 +35,43 @@ class NSWIndex:
         self.graph[idx] = set(neighbors)
         for neighbor in neighbors:
             self.graph[neighbor].add(idx)
+            
+        def k_nn_search(self, query, m, k):
+            temp_res = set()
+            candidates = set()
+            visited_set = set()
+            result = set()
+            for i in range(m):
+                candidates.add(self.get_random_entry_point())
+            temp_res.clear()
+
+            while candidates:
+                c = min(candidates, key=lambda x: self.distance(query, self.data[x]))
+                candidates.remove(c)
+
+                if c in visited_set or (
+                    result
+                    and self.distance(query, self.data[c])
+                    > self.distance(
+                        query,
+                        self.data[
+                            max(
+                                result, key=lambda x: self.distance(query, self.data[x])
+                            )
+                        ],
+                    )
+                ):
+
+                 break
+
+                visited_set.add(c)
+                temp_res.add(c)
+                for e in self.graph.get(c, []):
+                    if e not in visited_set:
+                        visited_set.add(e)
+                        candidates.add(e)
+                        temp_res.add(e)
+
+            result.update(temp_res)
+
+        return sorted(result, key=lambda x: self.distance(query, self.data[x]))[:k]
