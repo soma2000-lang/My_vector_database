b5eeec27277ff22eeb501776bdeebef5977dbadc
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 21:10:15 +0530

    Update at Wed, 26 Jun 2024 15:40:15 GMT

---
+++ tinydb/utils.py
@@ -0,0 +1,15 @@
+import os
+import numpy
+import shutil
+import tarfile
+import urllib.request as request
+
+from contextlib import closing
+
+from contextlib import closing
+
+
+DATA_PATH = os.path.join("data", "siftsmall", "siftsmall_base.fvecs")
+QUERY_PATH = os.path.join("data", "siftsmall", "siftsmall_query.fvecs")
+LABEL_PATH = os.path.join("data", "siftsmall", "siftsmall_groundtruth.ivecs")
+
