0938360696d4836715c92091f788f61f47241071
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 21:12:26 +0530

    Update at Wed, 26 Jun 2024 15:42:26 GMT

--- tinydb/utils.py
+++ tinydb/utils.py
@@ -13,3 +13,13 @@ DATA_PATH = os.path.join("data", "siftsmall", "siftsmall_base.fvecs")
 QUERY_PATH = os.path.join("data", "siftsmall", "siftsmall_query.fvecs")
 LABEL_PATH = os.path.join("data", "siftsmall", "siftsmall_groundtruth.ivecs")
 
+
+def read_vecs(path: str, ivecs: bool = False) -> numpy.ndarray:
+    a = numpy.fromfile(path, dtype="int32")
+    d = a[0]
+    matrix = a.reshape(-1, d + 1)[:, 1:].copy()
+
+    if not ivecs:
+        matrix = matrix.view("float32")
+
+    return matrix
