a6f6e1a9be42829fcc889ed6837fc7fb9f340d78
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 19:09:00 +0530

    Update at Wed, 26 Jun 2024 13:39:00 GMT

--- tinydb/hnsw.py
+++ tinydb/hnsw.py
@@ -6,3 +6,40 @@ from heapq import nlargest, nsmallest, heappop, heappush, heapify
 from tqdm import tqdm
 
 
+@dataclass
+class HNSWConfig:
+    M: int
+    M_max: int
+    M_max0: int
+    m_L: float
+    ef_construction: int
+    ef_search: int
+
+    neighbors: str = "simple"
+    extend_candidates: bool = False
+    keep_pruned_connections: bool = True
+
+
+DEFAULT_CONFIG = HNSWConfig(
+    M=16,
+    M_max=16,
+    M_max0=32,
+    m_L=(1.0 / math.log(16)),
+    ef_construction=32,
+    ef_search=32,
+)
+
+
+class HNSWIndex(Index):
+    def __init__(
+        self, d: int, distance: str = "cosine", config: HNSWConfig = DEFAULT_CONFIG
+    ) -> None:
+        super().__init__(d, distance)
+
+        self.config = config
+        self.vectors = None
+
+        self.ep = 0
+        self.L = 0
+        self.ix = 0
+        self.layers = [self.layer_factory(0, self.ep)]
