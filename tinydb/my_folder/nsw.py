b0ce926092bcdc1259eabf26b72f8907cd6e440a
Somasree <bishnupadamajumder32@gmail.com>
2024-06-26 21:52:47 +0530

    Update at Wed, 26 Jun 2024 16:22:47 GMT

--- tinydb/my_folder/nsw.py
+++ tinydb/my_folder/nsw.py
@@ -6,11 +6,22 @@ from tinydb.knn import cosine_similarity
 
 class NSWIndex:
     def __init__(self):
-        self.graph = {}  # Dictionary to store the graph
-        self.data = []  # List to store the actual data items
-
+        self.graph = {}  
+        self.data = []  
+        
     def add_item(self, item, f=10, w=5):
-        # Add item to data list
+        
         self.data.append(item)
         idx = len(self.data) - 1
+        if idx == 0:
+            self.graph[idx] = set()
+            return
+
+        # Perform k-NN search to find nearest neighbors
+        neighbors = self.k_nn_search(item, w, f)
+
+        # Connect the new item with its neighbors
+        self.graph[idx] = set(neighbors)
+        for neighbor in neighbors:
+            self.graph[neighbor].add(idx)
 
